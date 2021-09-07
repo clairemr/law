24b19fbd85d605ae606729cb8f5028e8071d7185
Claire Reincastle <claire.reincastle@iag.com.au>
2021-09-08 09:58:33 +1000

    update sidebar

--- guides.sidebar.js
+++ guides.sidebar.js
@@ -8,11 +8,6 @@ module.exports = {
       type: "category",
       label: "Getting Started",
       items: ["getting_started_bootstrap", "getting_started_core", "getting_started_extension", "getting_started_app"],
-    },
-    {
-      type: "category",
-      label: "Template Building",
-      items: ["building_template", "building_ecr_template", "building_lambda_template"],
-    },
+    }
   ],
 };
